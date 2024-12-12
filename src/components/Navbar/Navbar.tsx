f978a50a9a512caa7a38570c16aa378eea3769ea
ruanfrazao07 <ruanpablof7@gmail.com>
2024-12-12 13:45:28 -0300

    feat: Arquivos novos para adicionar elementos na navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+      <header>
+        <nav className="p-6">
+            <Link href="/">
+                  <Image src="assets/logo.svg" width={50} height={50} alt="Logo Frazão"/>
+            </Link>
+
+            <ul className="">
+                <li className="">
+                  <Link href="/" className="">
+                    Home
+                  </Link>
+                </li>
+
+                <li className="">
+                  <Link href="/sobre" className="">
+                    Sobre
+                  </Link>
+                </li>
+
+                <li className="">
+                  <Link href="/projetos" className="">
+                    Projetos
+                  </Link>
+                </li>
+
+            </ul>
+            <button className="">
+              Contatar
+            </button>
+        </nav>
+      </header>
+    );
+  }
+  export default Navbar;
